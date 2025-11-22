de3bc94689b72645a22dbbc528ab1e64c314823f
thyahuh84-bs <thyahuh.84@gmail.com>
2025-11-22 18:38:48 -0300

    Delete src directory

--- src/ContatoNaoEncontradoException.java
+++
@@ -1,4 +0,0 @@
-
-public class ContatoNaoEncontradoException extends Exception{
-
-}
--- src/GerenciadorContatos.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface GerenciadorContatos{
-    void adicionarContato(Contato contato)throws ContatoExistenteException;
-    Contato buscarContato(String name)throws ContatoNaoEncontradoException;
-    void removerContato(String name)throws ContatoNaoEncontradoException;
-    List<Contato> listarTodosContatos();
-}
